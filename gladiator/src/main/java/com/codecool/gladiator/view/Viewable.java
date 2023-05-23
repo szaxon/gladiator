d77bdaa4820790f0254b38eb28875b8d3c175209
szaxon <115426308+szaxon@users.noreply.github.com>
2023-05-23 10:17:22 +0200

    Add files via upload

---
+++ gladiator/src/main/java/com/codecool/gladiator/view/Viewable.java
@@ -0,0 +1,7 @@
+package com.codecool.gladiator.view;
+
+public interface Viewable {
+    void display(String message);
+
+    int getNumberBetween(int lowerBoundInclusive, int upperBoundExclusive);
+}
---
+++ gladiator/src/test/java/com/codecool/gladiator/view/ConsoleViewTest.java
@@ -0,0 +1,20 @@
+package com.codecool.gladiator.view;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleViewTest {
+
+    @Test
+    void testDisplay() {
+        assertEquals(1, 1);
+        //TODO: write more tests
+    }
+
+    @Test
+    void testGetNumberBetween() {
+        //TODO: write more tests
+    }
+}
