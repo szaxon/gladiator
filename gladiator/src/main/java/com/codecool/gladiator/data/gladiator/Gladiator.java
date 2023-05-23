1b4051b2f9e12824503af828b3393fc22300e3a1
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-05-23 16:16:57 +0200

    add statistics to the gladiator object and its subclasses

--- gladiator/src/main/java/com/codecool/gladiator/data/gladiator/Gladiator.java
+++ gladiator/src/main/java/com/codecool/gladiator/data/gladiator/Gladiator.java
@@ -5,56 +5,34 @@ import com.codecool.gladiator.data.StatisticMultiplier;
 public abstract class Gladiator {
 
     private final GladiatorName gladiatorName;
-
     private GladiatorType gladiatorType;
+    protected StatisticMultiplier hpMultiplier;
+    protected StatisticMultiplier spMultiplier;
+    protected StatisticMultiplier dexMultiplier;
+    protected double maxHp;
+    protected double maxSp;
+    protected double maxDex;
+    protected double currentHp;
 
-    private StatisticMultiplier hpMultiplier;
+    protected int level;
 
-    private double hp;
-
-    private StatisticMultiplier spMultiplier;
-
-    private double sp;
-
-    private StatisticMultiplier dexMultiplier;
-
-    private double dex;
-
-    private int health;
-
-    private int level;
-
-    public Gladiator(GladiatorName gladiatorName, GladiatorType gladiatorType) {
+    public Gladiator(GladiatorName gladiatorName, GladiatorType gladiatorType, int level) {
         this.gladiatorName = gladiatorName;
         this.gladiatorType = gladiatorType;
+        this.level = level;
     }
-
     public String getFullName() {
         return gladiatorType.toString() + gladiatorName.toString();
-    }
-
-    public int getLevel() {
-        return level;
     }
 
     public void levelUp() {
         level += 1;
     }
+    public double getMaxSp(){return maxSp;}
+    public double getMaxDex() {return maxDex;}
+    public double getMaxHp() {return maxHp;}
+    public void decreaseHp(double damage){currentHp -= damage;}
+
+    public double getCurrentHp() {return currentHp;}
+    public boolean isDefeated(){return currentHp < 0;}
 }
-
-
-//        Within the Gladiator class, there are three protected functions that return multipliers for the HP, SP,
-//        and DEX statistics, respectively. The functions MUST be implemented by every subclass of Gladiator.
-//
-//        Within the Gladiator class, there are three public functions that return the maximum available values of the gladiator's statistics (HP, SP, and DEX, respectively). The available value is calculated by the maximumStatistic = baseStatistic × statisticMultiplier × level formula.
-//
-//        Every gladiator has a current health value. This decreases during a combat when receiving hits from the enemy. When the value goes below zero, the gladiator dies.
-//
-//        There is a Swordsman subclass of Gladiator. Its multipliers are: HP: medium, SP: medium, DEX: medium
-//
-//        There is an Archer subclass of Gladiator. Its multipliers are: HP: medium, SP: medium, DEX: high.
-//
-//        There is an Assassin subclass of Gladiator. Its multipliers are: HP: low, SP: high, DEX: high.
-//
-//        There is a Brutal subclass of Gladiator. Its multipliers are: HP: high, SP: high, DEX: low.
-
--- gladiator/src/main/java/com/codecool/gladiator/data/gladiator/Swordsman.java
+++ gladiator/src/main/java/com/codecool/gladiator/data/gladiator/Swordsman.java
@@ -1,8 +1,13 @@
 package com.codecool.gladiator.data.gladiator;
 
+import com.codecool.gladiator.data.StatisticMultiplier;
+
 public class Swordsman extends Gladiator{
-    public Swordsman(GladiatorName gladiatorName, GladiatorType gladiatorType) {
-        super(gladiatorName, gladiatorType);
+    public Swordsman(GladiatorName gladiatorName, GladiatorType gladiatorType,int level, double hp, double sp, double dex) {
+        super(gladiatorName, gladiatorType, level);
+        maxDex = dex * StatisticMultiplier.MEDIUM.getMultiplier() * level;
+        maxHp = hp * StatisticMultiplier.MEDIUM.getMultiplier()* level;
+        maxSp = sp * StatisticMultiplier.MEDIUM.getMultiplier()* level;
     }
 
 }
