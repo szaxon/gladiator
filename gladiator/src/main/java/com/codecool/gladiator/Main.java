e6a5152769886589ee28c44c0d4a469232014415
László Szakszon <115426308+szaxon@users.noreply.github.com>
2023-05-24 18:18:40 +0200

    prepare application

--- gladiator/src/main/java/com/codecool/gladiator/Main.java
+++ gladiator/src/main/java/com/codecool/gladiator/Main.java
@@ -1,43 +1,36 @@
 package com.codecool.gladiator;
 
-import com.codecool.gladiator.data.gladiator.Archer;
-import com.codecool.gladiator.data.gladiator.Gladiator;
+import com.codecool.gladiator.data.gladiator.*;
 import com.codecool.gladiator.logic.Colosseum;
 import com.codecool.gladiator.logic.Combat;
 import com.codecool.gladiator.logic.tournament.Tournament;
 import com.codecool.gladiator.view.ConsoleView;
+import com.codecool.gladiator.view.Viewable;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        ConsoleView consoleView = new ConsoleView();
-
-        //consoleView.getNumberBetween(2, 11);
-
+        Viewable viewable = new ConsoleView();
         Colosseum colosseum = new Colosseum();
-        List<Gladiator> gladiatorList  = colosseum.generateGladiators(4);
-
-        colosseum.splitGladiatorsIntoPairs(gladiatorList);
-
-        Tournament tournament = new Tournament();
-        tournament.addAll(gladiatorList);
-        System.out.println(tournament.returnTree());
-
         Combat combat = new Combat();
+        Tournament tournament = new Tournament();
+        List<List<Gladiator>> gladiatorGroups = colosseum.generateGladiatorGroups(4);
 
+        armin(viewable, combat, tournament, gladiatorGroups);
+    }
 
+    private static void armin(Viewable viewable, Combat combat, Tournament tournament, List<List<Gladiator>> gladiatorGroups) {
+        for (List<Gladiator> gladiatorGroup : gladiatorGroups) {
+            tournament.addAll(gladiatorGroup);
+        }
 
+        viewable.display(tournament.returnTree());
 
-
-
-
-
-
-
-
-
-
+        for (List<Gladiator> gladiatorGroup : gladiatorGroups) {
+            Gladiator winner = combat.simulate(gladiatorGroup);
+            tournament.add(winner);
+            System.out.println(tournament.returnTree());
+        }
     }
 }
--- gladiator/src/main/java/com/codecool/gladiator/logic/Combat.java
+++ gladiator/src/main/java/com/codecool/gladiator/logic/Combat.java
@@ -2,6 +2,8 @@ package com.codecool.gladiator.logic;
 
 import com.codecool.gladiator.data.gladiator.Gladiator;
 import com.codecool.gladiator.logic.util.HitOrMiss;
+import com.codecool.gladiator.view.ConsoleView;
+import com.codecool.gladiator.view.Viewable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,25 +12,26 @@ public class Combat {
     private Gladiator firstAttacker;
     private Gladiator secondAttacker;
     private final List<String> combatLog = new ArrayList<>();
-
+    private Viewable viewable = new ConsoleView();
+    private final RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
     public Gladiator simulate(List<Gladiator> gladiatorPair) {
         if (gladiatorPair.get(0) == null) return gladiatorPair.get(1);
         else if (gladiatorPair.get(1) == null) return gladiatorPair.get(0);
         else if (gladiatorPair.get(0) == null && gladiatorPair.get(1) == null) return null;
 
-        selectFirstAttacker(gladiatorPair);
+        selectOrderOfAttackers(gladiatorPair);
 
         Colosseum colosseum = new Colosseum();
-        colosseum.starterMessage(firstAttacker, secondAttacker);
+        viewable.display(colosseum.starterMessage(firstAttacker, secondAttacker));
 
         while (!firstAttacker.isDefeated() || !secondAttacker.isDefeated()) {
             hitOrMiss(firstAttacker, secondAttacker);
             if (firstAttacker.isDefeated()) {
-                addToCombatLog(firstAttacker.getFullName() + " has died, " + secondAttacker.getFullName() + " wins!");
+                viewable.display(firstAttacker.getFullName() + " has died, " + secondAttacker.getFullName() + " wins!");
                 return secondAttacker;
             } else if (secondAttacker.isDefeated()) {
-                addToCombatLog(secondAttacker.getFullName() + " has died, " + firstAttacker.getFullName() + " wins!");
+                viewable.display(secondAttacker.getFullName() + " has died, " + firstAttacker.getFullName() + " wins!");
                 return firstAttacker;
             }
             hitOrMiss(secondAttacker, firstAttacker);
@@ -36,10 +39,10 @@ public class Combat {
         return null;
     }
 
-    private void selectFirstAttacker(List<Gladiator> gladiatorPair){
+    private void selectOrderOfAttackers(List<Gladiator> gladiatorPair){
         RandomNumberGenerator random = new RandomNumberGenerator();
         int firstAttackerIndex = random.generateRandom(0, 1);
-        int secondAttackerIndex = firstAttackerIndex == 1 ? 1 : 0;
+        int secondAttackerIndex = firstAttackerIndex == 1 ? 0 : 1;
         firstAttacker = gladiatorPair.get(firstAttackerIndex);
         secondAttacker = gladiatorPair.get(secondAttackerIndex);
     }
@@ -49,16 +52,17 @@ public class Combat {
         boolean hit = hitOrMiss.hitOrMiss(attacker, defender);
         if (hit) {
             double damage = calculateDamage(attacker);
+
             defender.decreaseHp(damage);
-            addToCombatLog(attacker.hitMessage(damage));
-        } else addToCombatLog(attacker.missMessage());
+            viewable.display(attacker.hitMessage(damage));
+        } else viewable.display(attacker.missMessage());
     }
 
     private double calculateDamage(Gladiator attacker) {
         double rangeStart = 0.1;
         double rangeEnd = 0.5;
-        RandomNumberGenerator random = new RandomNumberGenerator();
-        double multiplier = random.generateRandomDouble(rangeStart, rangeEnd);
+
+        double multiplier = randomNumberGenerator.generateRandomDouble(rangeStart, rangeEnd);
         return attacker.getMaxSp() * multiplier;
     }
 
@@ -66,9 +70,9 @@ public class Combat {
         return combatLog;
     }
 
-    private void addToCombatLog(String message) {
-        combatLog.add(message);
-    }
+//    private void addToCombatLog(String message) {
+//        combatLog.add(message);
+//    }
 
 
 }
