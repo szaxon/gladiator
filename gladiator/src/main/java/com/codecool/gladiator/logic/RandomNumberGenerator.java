bdc81c63ff66542ebf92271cfffd024b8dfeb3ad
matheanna <113627409+matheanna@users.noreply.github.com>
2023-05-24 12:13:13 +0200

    implement random double generator

--- gladiator/src/main/java/com/codecool/gladiator/logic/RandomNumberGenerator.java
+++ gladiator/src/main/java/com/codecool/gladiator/logic/RandomNumberGenerator.java
@@ -7,4 +7,8 @@ public class RandomNumberGenerator {
     public int generateRandom(int min, int max){
         return (int) ((Math.random() * (max - min)) + min);
     }
+    public double generateRandomDouble(double rangeStart, double rangeEnd){
+        Random random = new Random();
+        return rangeStart + (rangeEnd - rangeStart) * random.nextDouble();
+    }
 }
