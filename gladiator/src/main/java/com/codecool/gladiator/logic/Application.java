011e4b4198bffc1484c02973b65d33be6da31caa
matheanna <113627409+matheanna@users.noreply.github.com>
2023-05-25 09:38:26 +0200

    merge

--- gladiator/src/main/java/com/codecool/gladiator/logic/Application.java
+++ gladiator/src/main/java/com/codecool/gladiator/logic/Application.java
@@ -3,6 +3,7 @@ package com.codecool.gladiator.logic;
 import com.codecool.gladiator.data.gladiator.Gladiator;
 import com.codecool.gladiator.logic.tournament.Tournament;
 import com.codecool.gladiator.view.ConsoleView;
+import com.codecool.gladiator.view.DisplayColoredText;
 import com.codecool.gladiator.view.Gameplay;
 import com.codecool.gladiator.view.Viewable;
 
@@ -17,6 +18,7 @@ public class Application {
     private Tournament tournament = new Tournament();
     Colosseum colosseum = new Colosseum();
     public void run() {
+        new DisplayColoredText().displayImage();
         gladiatorGroups = colosseum.generateGladiatorGroups(4, null);
         for (List<Gladiator> gladiatorGroup : gladiatorGroups) {
             tournament.addAll(gladiatorGroup);
